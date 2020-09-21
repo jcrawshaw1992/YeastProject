20f6863f2209a2e2ae8537be863e622bf5d8d467
Jessica Crawshaw <jcrawshaw@9999L-201039-M.local>
2020-09-21 22:48:40 +1000

    21Sept2020

--- src/MembraneForces/HemeLBForce.cpp
+++ src/MembraneForces/HemeLBForce.cpp
@@ -134,7 +134,7 @@ void HemeLBForce<ELEMENT_DIM, SPACE_DIM>::ExecuteHemeLB()
     SystemOutput = std::system(run_hemelb_setup.c_str());
 
     // Step 2: Update xml file
-    PRINT_VARIABLE(double_to_string(mExpectedVelocity, 6), double_to_string(mEstimatedIC, 11)  )
+    PRINT_2_VARIABLES(double_to_string(mExpectedVelocity, 6), double_to_string(mEstimatedIC, 11)  )
     std::string update_xml_file = "python projects/VascularRemodelling/apps/update_xml_file.py -period "+std::to_string(Period) +" -directory " + mHemeLBDirectory + " -InitalConditions " + double_to_string(mEstimatedIC, 11) + " -ConvergenceTermination true -AverageVelocity " + double_to_string(mExpectedVelocity, 6); 
     SystemOutput = std::system(update_xml_file.c_str());
 // /Volumes/Backup Plus/ChasteWorkingDirectory/ShrunkPlexus/SetUpData/config.xml
