324eab052ccf6aa25e31f4ffe50be0dc22adc01a
JessC <jcrawshaw@student.unimelb.edu.au>
2021-04-06 13:54:24 +1000

    6thApril

--- src/HistoryDepMeshBasedCellPopulation.cpp
+++ src/HistoryDepMeshBasedCellPopulation.cpp
@@ -407,7 +407,7 @@ template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void HistoryDepMeshBasedCellPopulation<ELEMENT_DIM, SPACE_DIM>::TakeInPreAllocatedRemeshGeometry()
 {
     // std::string Remeshedvtu = mChasteOutputDirectory + "RemeshedGeometry.vtu";
-     std::string Remeshedvtu = "/data/vascrem/RemeshedGeometry.vtu";
+    mPreAllocatedRemeshedMesh = "/data/vascrem/RemeshedGeometry.vtu";
 
     // PRINT_VARIABLE(mChasteOutputDirectory)
     // bool copy_file(mPreAllocatedRemeshedMesh.c_str(), Remeshedvtu.c_str(), NULL, COPYFILE_DATA | COPYFILE_XATTR);
