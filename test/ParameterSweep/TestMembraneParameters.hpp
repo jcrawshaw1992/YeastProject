d6b1f35a2157947d2d99ccbb0253a8ec58a3e079
JessC <jcrawshaw@student.unimelb.edu.au>
2021-05-29 18:17:08 +1000

    Message

--- test/ParameterSweep/TestMembraneParameters.hpp
+++ test/ParameterSweep/TestMembraneParameters.hpp
@@ -134,58 +134,58 @@ public:
         // Load and fix any settings in the simulator
         OffLatticeSimulation<2, 3>* p_simulator = CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Load(ArchivedDirectory, 30);
 
-        /* Update the ouput directory for the population  */
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetChasteOutputDirectory(output_dir, EndTime);
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetStartTime(EndTime);
+        // /* Update the ouput directory for the population  */
+        // static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetChasteOutputDirectory(output_dir, EndTime);
+        // static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetStartTime(EndTime);
 
-        p_simulator->RemoveAllForces();
-        p_simulator->SetEndTime(EndTime + NewEndTime);
-        p_simulator->SetSamplingTimestepMultiple(SamplingTimestepMultiple);
-        p_simulator->SetDt(dt);
-        p_simulator->SetOutputDirectory(output_dir + ParameterSet);
+        // p_simulator->RemoveAllForces();
+        // p_simulator->SetEndTime(EndTime + NewEndTime);
+        // p_simulator->SetSamplingTimestepMultiple(SamplingTimestepMultiple);
+        // p_simulator->SetDt(dt);
+        // p_simulator->SetOutputDirectory(output_dir + ParameterSet);
 
-        /*
-        -----------------------------
-        Constant Compressive tissue pressure
-        ----------------------------
-        */
+        // /*
+        // -----------------------------
+        // Constant Compressive tissue pressure
+        // ----------------------------
+        // */
 
-        double P_blood = 0.002133152; // Pa ==   1.6004e-05 mmHg
-        double P_tissue = 0.001466542; // Pa == 1.5000e-05 mmHg , need to set up some collasping force for this -- this should be taken into consideration for the membrane properties :)
+        // double P_blood = 0.002133152; // Pa ==   1.6004e-05 mmHg
+        // double P_tissue = 0.001466542; // Pa == 1.5000e-05 mmHg , need to set up some collasping force for this -- this should be taken into consideration for the membrane properties :)
 
-        boost::shared_ptr<CylinderOutwardsPressure> p_ForceOut(new CylinderOutwardsPressure());
-        p_ForceOut->SetPressure(P_blood - P_tissue);
-        p_simulator->AddForce(p_ForceOut);
+        // boost::shared_ptr<CylinderOutwardsPressure> p_ForceOut(new CylinderOutwardsPressure());
+        // p_ForceOut->SetPressure(P_blood - P_tissue);
+        // p_simulator->AddForce(p_ForceOut);
 
-        /*
-        -----------------------------
-        Membrane forces
-        ----------------------------
-        */
-        boost::shared_ptr<MembraneDeformationForce> p_shear_force(new MembraneDeformationForce());
-        p_simulator->AddForce(p_shear_force);
+        // /*
+        // -----------------------------
+        // Membrane forces
+        // ----------------------------
+        // */
+        // boost::shared_ptr<MembraneDeformationForce> p_shear_force(new MembraneDeformationForce());
+        // p_simulator->AddForce(p_shear_force);
 
-        /* 
-        -----------------------------
-        Update membrane properties
-        ----------------------------
-        */
-        std::vector<boost::shared_ptr<AbstractCellBasedSimulationModifier<2, 3> > >::iterator iter = p_simulator->GetSimulationModifiers()->begin();
-        boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier = boost::static_pointer_cast<RemeshingTriggerOnHeteroMeshModifier<2, 3> >(*iter);
-        std::map<double, c_vector<long double, 4> > GrowthMaps;
-        GrowthMaps[1] = Create_c_vector(pow(10, -AreaParameter), pow(10, -DilationParameter), pow(10, -DeformationParamter), 0);
-        //                                          Strength,hetro,stepsize, setupsolve
-        p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 0, 100, 1);
+        // /* 
+        // -----------------------------
+        // Update membrane properties
+        // ----------------------------
+        // */
+        // std::vector<boost::shared_ptr<AbstractCellBasedSimulationModifier<2, 3> > >::iterator iter = p_simulator->GetSimulationModifiers()->begin();
+        // boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier = boost::static_pointer_cast<RemeshingTriggerOnHeteroMeshModifier<2, 3> >(*iter);
+        // std::map<double, c_vector<long double, 4> > GrowthMaps;
+        // GrowthMaps[1] = Create_c_vector(pow(10, -AreaParameter), pow(10, -DilationParameter), pow(10, -DeformationParamter), 0);
+        // //                                          Strength,hetro,stepsize, setupsolve
+        // p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 0, 100, 1);
 
-        // if (AreaParameter < (double)7 || DilationParameter < (double)7 || DeformationParamter < (double)7)
-        // {
-        //     p_Mesh_modifier->SetStartingParameterForSlowIncrease(StartingParameterForSlowIncrease);
-        //     p_Mesh_modifier->SetSlowIncreaseInMembraneStrength(1, 1);
-        // }
-        p_simulator->Solve();
-        CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(p_simulator);
-        SimulationTime::Instance()->Destroy();
-        SimulationTime::Instance()->SetStartTime(0.0);
+        // // if (AreaParameter < (double)7 || DilationParameter < (double)7 || DeformationParamter < (double)7)
+        // // {
+        // //     p_Mesh_modifier->SetStartingParameterForSlowIncrease(StartingParameterForSlowIncrease);
+        // //     p_Mesh_modifier->SetSlowIncreaseInMembraneStrength(1, 1);
+        // // }
+        // p_simulator->Solve();
+        // CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(p_simulator);
+        // SimulationTime::Instance()->Destroy();
+        // SimulationTime::Instance()->SetStartTime(0.0);
     }
 
 
