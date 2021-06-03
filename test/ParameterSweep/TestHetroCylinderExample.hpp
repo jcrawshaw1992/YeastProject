1fc9e9bd8c624145f5f69479ac0e13b5cf1a656b
JessC <jcrawshaw@student.unimelb.edu.au>
2021-06-03 18:42:23 +1000

    3thJune

--- test/ParameterSweep/TestHetroCylinderExample.hpp
+++ test/ParameterSweep/TestHetroCylinderExample.hpp
@@ -33,7 +33,7 @@
 class TestRemeshing : public AbstractCellBasedTestSuite
 {
 public:
-    void TestSetUpCylinderArchive() throw(Exception)
+    void offTestSetUpCylinderArchive() throw(Exception)
     {
         double EndTime = 5;
         double scale = 1e3;
@@ -144,65 +144,65 @@ public:
         OffLatticeSimulation<2, 3>* p_simulator = CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Load(Archieved, 5);
         // Load and fix any settings in the simulator
 
-        double scale = 1e3;
+        // double scale = 1e3;
 
-        c_vector<double, 3> UpperPlaneNormal =  Create_c_vector(0, 0, 1);
-        // c_vector<double, 3> UpperPlanePoint = Create_c_vector(12e-6 * scale, 0, 0);
-        c_vector<double, 3> UpperPlanePoint = Create_c_vector(0, 0, 12e-6 * scale);
-        c_vector<double, 3> LowerPlaneNormal =  Create_c_vector(0, 0, -1);
-        c_vector<double, 3> LowerPlanePoint = Create_c_vector(0, 0,28e-6 * scale);
+        // c_vector<double, 3> UpperPlaneNormal =  Create_c_vector(0, 0, 1);
+        // // c_vector<double, 3> UpperPlanePoint = Create_c_vector(12e-6 * scale, 0, 0);
+        // c_vector<double, 3> UpperPlanePoint = Create_c_vector(0, 0, 12e-6 * scale);
+        // c_vector<double, 3> LowerPlaneNormal =  Create_c_vector(0, 0, -1);
+        // c_vector<double, 3> LowerPlanePoint = Create_c_vector(0, 0,28e-6 * scale);
 
        
-        double DilationParameter=8.4;
-        double AreaParameter=7.5;
-        double DeformationParamter=8.3;
+        // double DilationParameter=8.4;
+        // double AreaParameter=7.5;
+        // double DeformationParamter=8.3;
      
-        double dt= 0.001;
-        double NewEndTime = 5;
-        double EndTime = 5;
+        // double dt= 0.001;
+        // double NewEndTime = 5;
+        // double EndTime = 5;
         
-        double SamplingTimestepMultiple = 100;
-        std::string output_dir = "HetroCylinder/";
+        // double SamplingTimestepMultiple = 100;
+        // std::string output_dir = "HetroCylinder/";
         
-        /* Update the ouput directory for the population  */
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetChasteOutputDirectory(output_dir, EndTime);
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetStartTime(EndTime);
+        // /* Update the ouput directory for the population  */
+        // static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetChasteOutputDirectory(output_dir, EndTime);
+        // static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetStartTime(EndTime);
 
-        p_simulator->RemoveAllForces();
-        p_simulator->SetEndTime(EndTime + NewEndTime);
-        p_simulator->SetSamplingTimestepMultiple(SamplingTimestepMultiple);
-        p_simulator->SetDt(dt);
-        p_simulator->SetOutputDirectory(output_dir);
+        // p_simulator->RemoveAllForces();
+        // p_simulator->SetEndTime(EndTime + NewEndTime);
+        // p_simulator->SetSamplingTimestepMultiple(SamplingTimestepMultiple);
+        // p_simulator->SetDt(dt);
+        // p_simulator->SetOutputDirectory(output_dir);
     
 
-        /*
-        -----------------------------
-        Membrane forces
-        ----------------------------
-        */
-        boost::shared_ptr<MembraneDeformationForceOnCylinder> p_shear_force(new MembraneDeformationForceOnCylinder());
-        p_simulator->AddForce(p_shear_force);
+        // /*
+        // -----------------------------
+        // Membrane forces
+        // ----------------------------
+        // */
+        // boost::shared_ptr<MembraneDeformationForceOnCylinder> p_shear_force(new MembraneDeformationForceOnCylinder());
+        // p_simulator->AddForce(p_shear_force);
 
-        /* 
-        -----------------------------
-        Update membrane properties
-        ----------------------------
-        */
-        std::vector<boost::shared_ptr<AbstractCellBasedSimulationModifier<2, 3> > >::iterator iter = p_simulator->GetSimulationModifiers()->begin();
-        boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier = boost::static_pointer_cast<RemeshingTriggerOnHeteroMeshModifier<2, 3> >(*iter);
-        std::map<double, c_vector<long double, 4> > GrowthMaps;
-        GrowthMaps[1] = Create_c_vector(pow(10, -AreaParameter), pow(10, -DilationParameter), pow(10, -DeformationParamter), 0);
-        GrowthMaps[0] = Create_c_vector(pow(10, -6), pow(10, -6), pow(10, -6), 0);
-        // Strength,hetro,stepsize, setupsolve
-        // GrowthMaps, Strength, Hetrogeneous,  StepSize,SetupSolve
-        p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 0, 1e-18, 1);
-        // p_Mesh_modifier->Boundaries(UpperPlaneNormal,UpperPlanePoint,  LowerPlaneNormal,LowerPlanePoint );
-        p_Mesh_modifier->SetBasementMembraneStrength(0);
-        // p_Mesh_modifier->SetStartingParameterForSlowIncrease(StartingParameterForSlowIncrease);
-        // p_Mesh_modifier->SetSlowIncreaseInMembraneStrength(1, 1);
+        // /* 
+        // -----------------------------
+        // Update membrane properties
+        // ----------------------------
+        // */
+        // std::vector<boost::shared_ptr<AbstractCellBasedSimulationModifier<2, 3> > >::iterator iter = p_simulator->GetSimulationModifiers()->begin();
+        // boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier = boost::static_pointer_cast<RemeshingTriggerOnHeteroMeshModifier<2, 3> >(*iter);
+        // std::map<double, c_vector<long double, 4> > GrowthMaps;
+        // GrowthMaps[1] = Create_c_vector(pow(10, -AreaParameter), pow(10, -DilationParameter), pow(10, -DeformationParamter), 0);
+        // GrowthMaps[0] = Create_c_vector(pow(10, -6), pow(10, -6), pow(10, -6), 0);
+        // // Strength,hetro,stepsize, setupsolve
+        // // GrowthMaps, Strength, Hetrogeneous,  StepSize,SetupSolve
+        // p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 0, 1e-18, 1);
+        // // p_Mesh_modifier->Boundaries(UpperPlaneNormal,UpperPlanePoint,  LowerPlaneNormal,LowerPlanePoint );
+        // p_Mesh_modifier->SetBasementMembraneStrength(0);
+        // // p_Mesh_modifier->SetStartingParameterForSlowIncrease(StartingParameterForSlowIncrease);
+        // // p_Mesh_modifier->SetSlowIncreaseInMembraneStrength(1, 1);
 
-        p_simulator->Solve();
-        CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(p_simulator);
+        // p_simulator->Solve();
+        // CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(p_simulator);
 
 
     }
