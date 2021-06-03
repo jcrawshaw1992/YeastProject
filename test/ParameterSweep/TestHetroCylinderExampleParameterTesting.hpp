5f49cf3960bb70477bd138668cbde6bbc97dc0cc
JessC <jcrawshaw@student.unimelb.edu.au>
2021-06-03 18:28:54 +1000

    3thJune

--- test/ParameterSweep/TestHetroCylinderExampleParameterTesting.hpp
+++ test/ParameterSweep/TestHetroCylinderExampleParameterTesting.hpp
@@ -60,7 +60,7 @@ public:
         HistoryDepMeshBasedCellPopulation<2, 3> cell_population(*mesh, cells);
         // cell_population.SetChasteOutputDirectory(output_dir, 0);
         // cell_population.SetWriteVtkAsPoints(true);
-        // cell_population.SetOutputMeshInVtk(true);
+        cell_population.SetOutputMeshInVtk(true);
         // Set population to output all data to results files
         cell_population.AddCellWriter<CellProliferativeTypesWriter>();
 
@@ -208,82 +208,81 @@ public:
     }
 
 
-    void offTestIntroduceHetro() throw(Exception)
-    {
+    // void offTestIntroduceHetro() throw(Exception)
+    // {
 
 
-        TS_ASSERT(CommandLineArguments::Instance()->OptionExists("-a"));
-        double a =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-a");
+    //     TS_ASSERT(CommandLineArguments::Instance()->OptionExists("-a"));
+    //     double a =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-a");
 
-        double b =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-b");
+    //     double b =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-b");
 
-        double p =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-p");   
-        double dt= 0.0001;
-         if (CommandLineArguments::Instance()->OptionExists("-dt"))
-        {
-            dt= CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-dt");
-        }  
-
-
-        std::stringstream out;
-        out << "A_"<< a << "_B_" <<b<< "_P_"<<p;
-        std::string ParameterSet = out.str();
-        std::string output_dir = "HetroCylinderParameters/"+ParameterSet;
+    //     double p =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-p");   
+    //     double dt= 0.0001;
+    //      if (CommandLineArguments::Instance()->OptionExists("-dt"))
+    //     {
+    //         dt= CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-dt");
+    //     }  
 
 
-        std::string Archieved = "HetroCylinderParameterTests/";
-        OffLatticeSimulation<2, 3>* p_simulator = CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Load(Archieved, 10);
-        // Load and fix any settings in the simulator
+    //     std::stringstream out;
+    //     out << "A_"<< a << "_B_" <<b<< "_P_"<<p;
+    //     std::string ParameterSet = out.str();
+    //     std::string output_dir = "HetroCylinderParameters/"+ParameterSet;
 
-        double scale = 1e3;
-        c_vector<double, 3> UpperPlaneNormal =  Create_c_vector(0, 0, 1);
-        // c_vector<double, 3> UpperPlanePoint = Create_c_vector(12e-6 * scale, 0, 0);
-        c_vector<double, 3> UpperPlanePoint = Create_c_vector(0, 0, 15e-6 * scale);
-        c_vector<double, 3> LowerPlaneNormal =  Create_c_vector(0, 0, -1);
-        c_vector<double, 3> LowerPlanePoint = Create_c_vector(0, 0,25e-6 * scale);
 
-        double DilationParameter=8.4;
-        double AreaParameter=7.5;
-        double DeformationParamter=8.3;
+    //     std::string Archieved = "HetroCylinderParameterTests/";
+    //     OffLatticeSimulation<2, 3>* p_simulator = CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Load(Archieved, 10);
+    //     // Load and fix any settings in the simulator
 
-        double NewEndTime = 50;
-        double EndTime = 10;
+    //     double scale = 1e3;
+    //     c_vector<double, 3> UpperPlaneNormal =  Create_c_vector(0, 0, 1);
+    //     // c_vector<double, 3> UpperPlanePoint = Create_c_vector(12e-6 * scale, 0, 0);
+    //     c_vector<double, 3> UpperPlanePoint = Create_c_vector(0, 0, 15e-6 * scale);
+    //     c_vector<double, 3> LowerPlaneNormal =  Create_c_vector(0, 0, -1);
+    //     c_vector<double, 3> LowerPlanePoint = Create_c_vector(0, 0,25e-6 * scale);
+
+    //     double DilationParameter=8.4;
+    //     double AreaParameter=7.5;
+    //     double DeformationParamter=8.3;
+
+    //     double NewEndTime = 50;
+    //     double EndTime = 10;
         
-        double SamplingTimestepMultiple = 10000;
+    //     double SamplingTimestepMultiple = 10000;
         
-        /* Update the ouput directory for the population  */
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetChasteOutputDirectory(output_dir, EndTime);
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetStartTime(EndTime);
-        static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetOutputMeshInVtk(true);
+    //     /* Update the ouput directory for the population  */
+    //     static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetChasteOutputDirectory(output_dir, EndTime);
+    //     static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetStartTime(EndTime);
 
-        p_simulator->SetEndTime(EndTime + NewEndTime);
-        p_simulator->SetSamplingTimestepMultiple(SamplingTimestepMultiple);
-        p_simulator->SetDt(dt);
-        p_simulator->SetOutputDirectory(output_dir);
+    //     p_simulator->SetEndTime(EndTime + NewEndTime);
+    //     p_simulator->SetSamplingTimestepMultiple(SamplingTimestepMultiple);
+    //     p_simulator->SetDt(dt);
+    //     p_simulator->SetOutputDirectory(output_dir);
 
-        /* 
-        -----------------------------
-        Update membrane properties
-        ----------------------------
-        */
-        std::vector<boost::shared_ptr<AbstractCellBasedSimulationModifier<2, 3> > >::iterator iter = p_simulator->GetSimulationModifiers()->begin();
-        boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier = boost::static_pointer_cast<RemeshingTriggerOnHeteroMeshModifier<2, 3> >(*iter);
-        std::map<double, c_vector<long double, 4> > GrowthMaps;
-        GrowthMaps[1] = Create_c_vector(pow(10, -AreaParameter), pow(10, -DilationParameter), pow(10, -DeformationParamter), 0);
-        GrowthMaps[0] = Create_c_vector(pow(10, -6), pow(10, -6), pow(10, -5), 0);
-        // Strength,hetro,stepsize, setupsolve
-        // GrowthMaps, Strength, Hetrogeneous,  StepSize,SetupSolve
-        p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 1, pow(10,-p), 1);
-        p_Mesh_modifier->Boundaries(UpperPlaneNormal,UpperPlanePoint,  LowerPlaneNormal,LowerPlanePoint );
-        p_Mesh_modifier->SetBasementMembraneStrength(0);
-        p_Mesh_modifier->SetPlateauParameters(a, b);
-        p_Mesh_modifier->SetUpdateFrequency(100);
+    //     /* 
+    //     -----------------------------
+    //     Update membrane properties
+    //     ----------------------------
+    //     */
+    //     std::vector<boost::shared_ptr<AbstractCellBasedSimulationModifier<2, 3> > >::iterator iter = p_simulator->GetSimulationModifiers()->begin();
+    //     boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier = boost::static_pointer_cast<RemeshingTriggerOnHeteroMeshModifier<2, 3> >(*iter);
+    //     std::map<double, c_vector<long double, 4> > GrowthMaps;
+    //     GrowthMaps[1] = Create_c_vector(pow(10, -AreaParameter), pow(10, -DilationParameter), pow(10, -DeformationParamter), 0);
+    //     GrowthMaps[0] = Create_c_vector(pow(10, -6), pow(10, -6), pow(10, -5), 0);
+    //     // Strength,hetro,stepsize, setupsolve
+    //     // GrowthMaps, Strength, Hetrogeneous,  StepSize,SetupSolve
+    //     p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 1, pow(10,-p), 1);
+    //     p_Mesh_modifier->Boundaries(UpperPlaneNormal,UpperPlanePoint,  LowerPlaneNormal,LowerPlanePoint );
+    //     p_Mesh_modifier->SetBasementMembraneStrength(0);
+    //     p_Mesh_modifier->SetPlateauParameters(a, b);
+    //     p_Mesh_modifier->SetUpdateFrequency(100);
 
-        p_simulator->Solve();
-        CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(p_simulator);
+    //     p_simulator->Solve();
+    //     CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(p_simulator);
 
 
-    }
+    // }
 
 
 
