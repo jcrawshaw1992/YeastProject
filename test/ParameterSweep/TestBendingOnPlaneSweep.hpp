21e0742b3be0dcf56912ef191af5ab380c258653
JessC <jcrawshaw@student.unimelb.edu.au>
2021-06-04 14:18:44 +1000

    l

--- test/ParameterSweep/TestBendingOnPlaneSweep.hpp
+++ test/ParameterSweep/TestBendingOnPlaneSweep.hpp
@@ -41,75 +41,75 @@
 class TestRemeshing : public AbstractCellBasedTestSuite
 {
 public:
-    void offTestBending() throw(Exception)
-    {
-        double Nc =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-Nc");
-        double AspectRatio =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-AspectRatio");
+    // void offTestBending() throw(Exception)
+    // {
+    //     double Nc =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-Nc");
+    //     double AspectRatio =CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-AspectRatio");
 
-        double BendingParameter = 9; //CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-BendingParameter");
-        double dt = 0.005; //For most using 0.001, but for apsect ratio 3 and refinemnt 30 need finer
+    //     double BendingParameter = 9; //CommandLineArguments::Instance()->GetDoubleCorrespondingToOption("-BendingParameter");
+    //     double dt = 0.005; //For most using 0.001, but for apsect ratio 3 and refinemnt 30 need finer
 
-        double EndTime = 600;
+    //     double EndTime = 600;
 
-        double SamplingTimestepMultiple = 1000; //2000;
+    //     double SamplingTimestepMultiple = 1000; //2000;
 
-        std::stringstream out;
-        out << "AspectRatio" << AspectRatio << "/Refinement" << Nc;
-        std::string ParameterSet = out.str();
-        std::string output_dir = "BendingForceOnBentRectanlge2/" + ParameterSet;
+    //     std::stringstream out;
+    //     out << "AspectRatio" << AspectRatio << "/Refinement" << Nc;
+    //     std::string ParameterSet = out.str();
+    //     std::string output_dir = "BendingForceOnBentRectanlge2/" + ParameterSet;
 
 
-        Honeycomb3DMeshGeneratorBentRectangle generator(Nc, Nc* AspectRatio, 1e-3, 1e-3);
-        MutableMesh<2, 3>* p_mesh = generator.GetMesh();
+    //     Honeycomb3DMeshGeneratorBentRectangle generator(Nc, Nc* AspectRatio, 1e-3, 1e-3);
+    //     MutableMesh<2, 3>* p_mesh = generator.GetMesh();
 
-        // Create the cells
-        MAKE_PTR(DifferentiatedCellProliferativeType, p_differentiated_type);
-        std::vector<CellPtr> cells;
-        CellsGenerator<FixedG1GenerationalCellCycleModel, 2> cells_generator;
-        cells_generator.GenerateBasicRandom(cells, p_mesh->GetNumNodes(), p_differentiated_type);
+    //     // Create the cells
+    //     MAKE_PTR(DifferentiatedCellProliferativeType, p_differentiated_type);
+    //     std::vector<CellPtr> cells;
+    //     CellsGenerator<FixedG1GenerationalCellCycleModel, 2> cells_generator;
+    //     cells_generator.GenerateBasicRandom(cells, p_mesh->GetNumNodes(), p_differentiated_type);
 
-        // Create a cell population
-        HistoryDepMeshBasedCellPopulation<2, 3> cell_population(*p_mesh, cells);
-        cell_population.SetChasteOutputDirectory(output_dir, 0);
-        cell_population.AddPopulationWriter<ElementAnglesWriter>();
+    //     // Create a cell population
+    //     HistoryDepMeshBasedCellPopulation<2, 3> cell_population(*p_mesh, cells);
+    //     cell_population.SetChasteOutputDirectory(output_dir, 0);
+    //     cell_population.AddPopulationWriter<ElementAnglesWriter>();
 
-        // Set population to output all data to results files
-        cell_population.AddCellWriter<CellProliferativeTypesWriter>();
+    //     // Set population to output all data to results files
+    //     cell_population.AddCellWriter<CellProliferativeTypesWriter>();
 
-        OffLatticeSimulation<2, 3> simulator(cell_population);
-        simulator.SetOutputDirectory(output_dir);
-        simulator.SetSamplingTimestepMultiple(SamplingTimestepMultiple);
-        simulator.SetDt(dt);
-        simulator.SetUpdateCellPopulationRule(false);
-        simulator.SetEndTime(EndTime);
+    //     OffLatticeSimulation<2, 3> simulator(cell_population);
+    //     simulator.SetOutputDirectory(output_dir);
+    //     simulator.SetSamplingTimestepMultiple(SamplingTimestepMultiple);
+    //     simulator.SetDt(dt);
+    //     simulator.SetUpdateCellPopulationRule(false);
+    //     simulator.SetEndTime(EndTime);
 
-        boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier(new RemeshingTriggerOnHeteroMeshModifier<2, 3>());
-        std::map<double, c_vector<long double, 4> > GrowthMaps;
-        GrowthMaps[1] = Create_c_vector(pow(10, -7),0,0, pow(10, -BendingParameter));
-        //Strength , hetro, stepsize, setupsolve
-        p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 0, 100, 1);
-        simulator.AddSimulationModifier(p_Mesh_modifier);
+    //     boost::shared_ptr<RemeshingTriggerOnHeteroMeshModifier<2, 3> > p_Mesh_modifier(new RemeshingTriggerOnHeteroMeshModifier<2, 3>());
+    //     std::map<double, c_vector<long double, 4> > GrowthMaps;
+    //     GrowthMaps[1] = Create_c_vector(pow(10, -7),0,0, pow(10, -BendingParameter));
+    //     //Strength , hetro, stepsize, setupsolve
+    //     p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1, 0, 100, 1);
+    //     simulator.AddSimulationModifier(p_Mesh_modifier);
 
-        /*
-        -----------------------------
-        Bending forces
-        ----------------------------
-        */
-        boost::shared_ptr<MembraneBendingForce0TargetAngle> p_membrane_force(new MembraneBendingForce0TargetAngle());
-        p_membrane_force->SetMembraneStiffness(pow(10, -BendingParameter));
-        simulator.AddForce(p_membrane_force);
+    //     /*
+    //     -----------------------------
+    //     Bending forces
+    //     ----------------------------
+    //     */
+    //     boost::shared_ptr<MembraneBendingForce0TargetAngle> p_membrane_force(new MembraneBendingForce0TargetAngle());
+    //     p_membrane_force->SetMembraneStiffness(pow(10, -BendingParameter));
+    //     simulator.AddForce(p_membrane_force);
 
-        /*
-        -----------------------------
-        Membrane forces
-        ----------------------------
-        */
-        boost::shared_ptr<MembraneDeformationForce> p_shear_force(new MembraneDeformationForce());
-        simulator.AddForce(p_shear_force);
+    //     /*
+    //     -----------------------------
+    //     Membrane forces
+    //     ----------------------------
+    //     */
+    //     boost::shared_ptr<MembraneDeformationForce> p_shear_force(new MembraneDeformationForce());
+    //     simulator.AddForce(p_shear_force);
 
-        simulator.Solve();
-        CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(&simulator);
-    }
+    //     simulator.Solve();
+    //     CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Save(&simulator);
+    // }
 
 
 
@@ -126,12 +126,18 @@ public:
 
         double SamplingTimestepMultiple = 1000; //2000;
 
-        std::stringstream out;
+        // std::stringstream out;
+        // out << "AspectRatio" << AspectRatio << "/Refinement" << Nc;
+        // std::string ParameterSet = out.str();
+        // std::string output_dir = "BendingForceOnBentRectanlge2/" + ParameterSet;
+
+
+         std::stringstream out;
         out << "AspectRatio" << AspectRatio << "/Refinement" << Nc;
         std::string ParameterSet = out.str();
-        std::string output_dir = "BendingForceOnBentRectanlge/" + ParameterSet;
+        std::string output_dir = "BendingForceOnBentRectanlge2/" + ParameterSet;
 
-        OffLatticeSimulation<2, 3>* p_simulator = CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Load(output_dir, EndTime);
+        OffLatticeSimulation<2, 3>* p_simulator = CellBasedSimulationArchiver<2, OffLatticeSimulation<2, 3>, 3>::Load("BendingForceOnBentRectanlge2/" + ParameterSet, EndTime);
 
 
         /* Update the ouput directory for the population  */
