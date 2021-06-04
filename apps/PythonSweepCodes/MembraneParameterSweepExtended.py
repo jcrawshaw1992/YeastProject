6426d082a49d0b6d681217c36cb2b94af4edd707
JessC <jcrawshaw@student.unimelb.edu.au>
2021-06-04 12:53:59 +1000

    4June

--- apps/PythonSweepCodes/MembraneParameterSweepExtended.py
+++ apps/PythonSweepCodes/MembraneParameterSweepExtended.py
@@ -17,10 +17,10 @@ from os import path
 if __name__=="__main__":
 
     t0 = time.time()
-    GenerateRunner =1
-    if GenerateRunner ==1:
-        command = "cd ~/Chaste && scons b=GccOpt projects/VascularRemodelling/test/ParameterSweep/TestMembraneParameterSweep.hpp"
-        subprocess.call(command, shell=True)
+    # GenerateRunner =1
+    # if GenerateRunner ==1:
+    #     command = "cd ~/Chaste && scons b=GccOpt projects/VascularRemodelling/test/ParameterSweep/TestMembraneParameterSweep.hpp"
+    #     subprocess.call(command, shell=True)
 
     chaste_run_exe = '/home/vascrem/Chaste/projects/VascularRemodelling/build/optimised/ParameterSweep/TestMembraneParameterSweepRunner '
     TerminalOutputFolder = "/data/vascrem/testoutput/MembraneParameterSweep/Cylinder/SweepTerminalOutputsNew/"
@@ -29,19 +29,16 @@ if __name__=="__main__":
         os.mkdir(TerminalOutputFolder)
 
     # Parameters = [[10, 9,  9],   [10, 9, 10],   [9, 9, 9],   [9, 9, 10],   [9, 9.5, 9],   [9, 9.5, 10],   [9, 10, 9],   [9, 10, 10],  [8, 9,  9], [8, 9, 10], [8, 9.5, 9], [8, 9.5, 10], [8, 10, 9], [8, 10, 10], [7, 8.5, 9], [7, 8.5, 10], [7, 9, 9], [7, 9, 10], [7, 9.5, 9], [7, 9.5, 10], [7, 10, 9], [7, 10, 10]]
-    Parameters = [[9,  9.5,  10] , [9, 10, 10] , [8,  9.5,  10] , [8, 10, 10] , [7,    9.5,  10] , [7,   10, 10] ] 
+    # Parameters = [[9,  9.5,  10] , [9, 10, 10] , [8,  9.5,  10] , [8, 10, 10] , [7,    9.5,  10] , [7,   10, 10] ] 
+    Parameters = [[9, 10, 10] ,  [8, 10, 10] ] 
     
     print len(Parameters)
     for I in Parameters:
         i = I[0]
         j = I[1]
         k = I[2]
-        dt = ' 0.0005'
-        if i <7.3 or j<7.3 or k<7.3:
-            SimulationTime = ' 120'
-            dt = ' 0.0001'
-        else:
-            SimulationTime = ' 120'
+        dt = ' 0.02'
+        SimulationTime = ' 50'
 
         Input1 = chaste_run_exe +' -AreaParameter '+str(i)+' -DilationParameter '+str(j)+' -DeformationParamter ' +str(k) +' -dt'+dt+' -NewEndTime'+SimulationTime
         Input2 = TerminalOutputFolder+'AreaParameter'+str(i)+'_DilationParameter'+str(j)+'_DeformationParamter' +str(k)+'.txt'
@@ -51,7 +48,7 @@ if __name__=="__main__":
 
     
     # OldFolder = "/data/vascrem/testoutput/MembraneParameterSweep/Cylinder/Parameters/"
-    # NewFolder = "/data/vascrem/testoutput/MembraneParameterSweep/Cylinder/CollectedResultsNew/"
+    # NewFolder = "/data/vascrem/testoutput/MembraneParameterSweep/Cylinder/CollectedResultsNewNew/"
     # os.mkdir(NewFolder)
 
     
