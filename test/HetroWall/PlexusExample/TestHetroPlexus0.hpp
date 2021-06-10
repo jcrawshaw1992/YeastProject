79ea0dc7b5f9b4cc55f4521a7c39da62b3c0550b
JessC <jcrawshaw@student.unimelb.edu.au>
2021-06-10 12:09:37 +1000

    s

--- test/HetroWall/PlexusExample/TestHetroPlexus0.hpp
+++ test/HetroWall/PlexusExample/TestHetroPlexus0.hpp
@@ -95,7 +95,7 @@ public:
         double P_tissue = 0.001466542; // Pa == 1.5000e-05 mmHg , need to set up some collasping force for this -- this should be taken into consideration for the membrane properties :)
 
         boost::shared_ptr<OutwardsPressure> p_ForceOut(new OutwardsPressure());
-        p_ForceOut->SetPressure((P_blood - P_tissue)/2);
+        p_ForceOut->SetPressure(2*(P_blood - P_tissue)/3);
         simulator.AddForce(p_ForceOut);
 
 
