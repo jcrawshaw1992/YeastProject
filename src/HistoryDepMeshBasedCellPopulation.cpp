d63ca9f05dbff1af63bea6477615046d0375858c
JessC <jcrawshaw@student.unimelb.edu.au>
2021-06-25 23:08:48 +1000

    s

--- src/HistoryDepMeshBasedCellPopulation.cpp
+++ src/HistoryDepMeshBasedCellPopulation.cpp
@@ -857,7 +857,7 @@ double HistoryDepMeshBasedCellPopulation<ELEMENT_DIM, SPACE_DIM>::GetClosestElem
         // c_vector<double, SPACE_DIM> Centroid = mCentroidMap[*elem_index];
         // unsigned elem_index = elem_iter->GetIndex();
         double DistanceFromContainingElement = DistanceBetweenPointAndElement(NewNodeLocation, *elem_index) ;
-        PRINT_VARIABLE(DistanceFromContainingElement)
+        // PRINT_VARIABLE(DistanceFromContainingElement)
         if (std::abs(DistanceFromContainingElement) < ClosestElementDistance)
         {
             ClosestElementDistance = std::abs(DistanceFromContainingElement);
