a6ecefeac7b78289d03b5f737d015b72e2cb7eb5
JessC <jcrawshaw@student.unimelb.edu.au>
2021-07-19 16:43:39 +1000

    s

--- src/Modifiers/RemeshingTriggerOnStepHeteroModifier.cpp
+++ src/Modifiers/RemeshingTriggerOnStepHeteroModifier.cpp
@@ -43,7 +43,7 @@ RemeshingTriggerOnStepHeteroModifier<ELEMENT_DIM, SPACE_DIM>::RemeshingTriggerOn
         // mGrowthMaps =  { {1, Create_c_vector(pow(10, -7.5), pow(10, -9), pow(10, -8.1), 1e-11) },// INitial state
         // mGrowthMaps =  { {1, Create_c_vector(pow(10, -8), pow(10, -9), pow(10, -9), 1e-11) },// Allowed deformation
         //  mGrowthMaps =  { {1, Create_c_vector(pow(10, -6), pow(10, -7), pow(10, -7.5), 1e-11) },
-        {0, Create_c_vector(pow(10, -7), pow(10, -6), pow(10, -5), 1e-11)}// {0, Create_c_vector(pow(10, -7), pow(10, -6), pow(10, -5), 1e-10)}
+        {0, Create_c_vector(pow(10, -7), pow(10, -6), pow(10, -15), 1e-11)}// {0, Create_c_vector(pow(10, -7), pow(10, -6), pow(10, -5), 1e-10)}
          };
 
     PRINT_VECTOR(mGrowthMaps[0])
