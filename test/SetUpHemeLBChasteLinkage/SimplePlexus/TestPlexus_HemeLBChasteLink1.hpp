517110cd9bd70091fa0912f500a7248f68d960e3
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-11 18:30:53 +1000

    s

--- test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_HemeLBChasteLink1.hpp
+++ test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_HemeLBChasteLink1.hpp
@@ -51,7 +51,7 @@ public:
         // double BendingParameter = -12;
 
 
-         double DilationParameter = -6;x
+        double DilationParameter = -6;x
         double AreaParameter = -6.5;
         double DeformationParamter = -6;
         double BendingParameter = -6;
@@ -66,7 +66,7 @@ public:
         double RemeshingTime = 10000;
         double EdgeLength =0.00045;
         
-        std::string output_dir = "SimpleHemeLBPlexus/LatestTry/";
+        std::string output_dir = "SimpleHemeLBPlexus/Collapse1/";
      
         std::string mesh_file = "/data/vascrem/testoutput/DeformingPlexus/FlatForceFINAL9/results_from_time_3/mesh_50.vtu";
         VtkMeshReader<2, 3> mesh_reader(mesh_file);
@@ -116,7 +116,7 @@ public:
         p_Mesh_modifier->TurnOffRemeshing();   
         //AreaConstant           AreaDilationModulus        ShearModulus
         std::map<double, c_vector<long double, 4> > GrowthMaps = { { 1, Create_c_vector(pow(10, AreaParameter), pow(10, DilationParameter), pow(10, DeformationParamter), pow(10, BendingParameter)) },
-                                                                    {0,  Create_c_vector(pow(10, -4), pow(10, -4), pow(10, -4),pow(10, BendingParameter))}    };
+                                                                    {0,  Create_c_vector(pow(10, -4), pow(10, -4), pow(10, -4),pow(10, BendingParameter))} };
  
         p_Mesh_modifier->SetMembraneStrength(1);
         p_Mesh_modifier->SetMembranePropeties(GrowthMaps, 1);
