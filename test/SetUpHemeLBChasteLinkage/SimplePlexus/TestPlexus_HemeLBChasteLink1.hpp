f99e19ee5a2432bfeed2b49bcc504ad4ed4a6a8b
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-11 16:56:09 +1000

    s

--- test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_HemeLBChasteLink1.hpp
+++ test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_HemeLBChasteLink1.hpp
@@ -48,7 +48,7 @@ public:
         double DilationParameter = -6;
         double AreaParameter = -5.5;
         double DeformationParamter = -6;
-        double BendingParameter = -7;
+        double BendingParameter = -8;
 
 
         TRACE("Jess is good")
