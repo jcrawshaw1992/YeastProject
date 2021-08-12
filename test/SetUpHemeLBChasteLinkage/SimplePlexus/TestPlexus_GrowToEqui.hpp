c67c813489d1e2841eb7905e1bbcb9db9f80a04e
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-12 14:57:01 +1000

    s

--- test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_GrowToEqui.hpp
+++ test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_GrowToEqui.hpp
@@ -231,7 +231,7 @@ public:
 
   void TestWithHemeLBForce() throw(Exception)
    {
-        double AreaParameter = -5.5;  double DilationParameter = -6; double DeformationParamter = -5.5; double BendingParameter = -11;
+        double AreaParameter = -5;  double DilationParameter = -6; double DeformationParamter = -5; double BendingParameter = -10;
         std::map<double, c_vector<long double, 4> > GrowthMaps = { { 1, Create_c_vector(pow(10, AreaParameter), pow(10, DilationParameter), pow(10, DeformationParamter), pow(10, BendingParameter)) }, {0,  Create_c_vector(pow(10, -4), pow(10, -4), pow(10, -4),pow(10, BendingParameter))} };
 
         std::string Archieved = "SimpleHemeLBPlexus2/GrowingToEqui3/";//std::string mesh_file = "/data/vascrem/testoutput/DeformingPlexus/FlatForceFINAL9/results_from_time_3/mesh_50.vtu";
