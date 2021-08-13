27cd67526443515840074707918f305f8375d0bb
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-13 23:12:08 +1000

    s

--- src/MembraneForces/HemeLBForce.cpp
+++ src/MembraneForces/HemeLBForce.cpp
@@ -993,10 +993,10 @@ void HemeLBForce<ELEMENT_DIM, SPACE_DIM>::UpdateCellData(AbstractCellPopulation<
 		cell_iter->GetCellData()->SetItem("HemeLBForce", Pressure);
         cell_iter->GetCellData()->SetItem("shear_stress", norm_2(shear_stress));
         PRINT_3_VARIABLES(mMinSS,  0.3*(mMaxSS- mMinSS) ,mMaxSS )
-        if ( norm_2(shear_stress)> (mMaxSS + 0.3*(mMaxSS- mMinSS)    )  )
+        if ( norm_2(shear_stress)> (mMaxSS + 1*(mMaxSS- mMinSS)    )  )
         {
             cell_iter->GetCellData()->SetItem("WallShearStressExtremes", 1);
-        }else if ( norm_2(shear_stress)<mMinSS -  0.3*(mMaxSS- mMinSS) )
+        }else if ( norm_2(shear_stress)<mMinSS -  0.5*(mMaxSS- mMinSS) )
         {
             cell_iter->GetCellData()->SetItem("WallShearStressExtremes", -1);
         }
