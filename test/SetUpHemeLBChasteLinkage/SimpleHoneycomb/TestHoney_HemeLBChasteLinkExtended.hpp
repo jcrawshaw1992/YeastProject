6458469ed091984cf2c6faa34f192c16af05cc27
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-17 14:42:09 +1000

    d

--- test/SetUpHemeLBChasteLinkage/SimpleHoneycomb/TestHoney_HemeLBChasteLinkExtended.hpp
+++ test/SetUpHemeLBChasteLinkage/SimpleHoneycomb/TestHoney_HemeLBChasteLinkExtended.hpp
@@ -55,7 +55,7 @@ public:
         std::string Archieved =  "FSISimulations/Honey/Collapse1_StrongMembraneParameterVariationAdditionalInitialConditionCollapseMoreRemeshing2/";
 
         
-        double EndTime = 11.1;
+        double EndTime = 11.9;
         double SamplingStep = 50;
         double dt = 0.001;
         double RemeshingTime = 500;//50;
