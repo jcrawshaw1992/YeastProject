bb29b831babd6c376ff216225125c7e6dda38ed7
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-23 20:08:49 +1000

    s

--- test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_HemeLBChasteLink2Type2.hpp
+++ test/SetUpHemeLBChasteLinkage/SimplePlexus/TestPlexus_HemeLBChasteLink2Type2.hpp
@@ -48,10 +48,10 @@ public:
          double AreaParameter = -5;  double DilationParameter = -5.5; double DeformationParamter = -5; double BendingParameter = -7;
         std::map<double, c_vector<long double, 4> > GrowthMaps = { { 1, Create_c_vector(pow(10, AreaParameter), pow(10, DilationParameter), pow(10, DeformationParamter), pow(10, BendingParameter)) }, {0,  Create_c_vector(pow(10, -4), pow(10, -4), pow(10, -4),pow(10, BendingParameter))} };
 
-        std::string Archieved = "FSISimulations/Plexus/Collapse2";//"FSISimulations/Plexus/EquiWithHemeLB3/";
+        std::string Archieved ="FSISimulations/Plexus/EquiWithHemeLB3/";
         std::string output_dir = "FSISimulations/Plexus/Collapse2/Type2/";
 
-        double EndTime = 12.1;
+        double EndTime = 12;
         double SamplingStep = 50;
         double dt = 0.0001;
         double RemeshingTime = 600 ;
@@ -90,7 +90,7 @@ public:
         static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetRemeshingSoftwear("CGAL");
         static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).SetOperatingSystem("server");
 
-         static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).ExecuteHistoryDependentRemeshing();
+        //  static_cast<HistoryDepMeshBasedCellPopulation<2, 3>&>(p_simulator->rGetCellPopulation()).ExecuteHistoryDependentRemeshing();
    
 
         p_simulator->SetSamplingTimestepMultiple(SamplingStep);
