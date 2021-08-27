a0e696f75121d60e488914e10cb6664da85660ec
JessC <jcrawshaw@student.unimelb.edu.au>
2021-08-27 15:41:55 +1000

    s

--- src/MembraneForces/HemeLBForce.cpp
+++ src/MembraneForces/HemeLBForce.cpp
@@ -1025,12 +1025,15 @@ void HemeLBForce::UpdateCellData(AbstractCellPopulation<2,3>& rCellPopulation)
 			}
             if (distance <  mRegionOfForceCollection)
             {
-                // shear_stress +=mAppliedTangentTractions[fluid_site_index];
+                assert(std::isnan(mAppliedTangentTractions[fluid_site_index][0] ==0 &&  std::isnan(mAppliedTangentTractions[fluid_site_index][1] ==0  &&  std::isnan(mAppliedTangentTractions[fluid_site_index][2] ==0)
+                shear_stress +=mAppliedTangentTractions[fluid_site_index];
 
                 //   std::map<unsigned, c_vector<double, 3>> mAppliedTangentTractions;
-                shear_stress[0] =shear_stress[0]  +mAppliedTangentTractions[fluid_site_index][0];
-                shear_stress[1] = shear_stress[1]  +mAppliedTangentTractions[fluid_site_index][1];
-                shear_stress[2] = shear_stress[2]  + mAppliedTangentTractions[fluid_site_index][2];
+                // shear_stress[0] =shear_stress[0]  +mAppliedTangentTractions[fluid_site_index][0];
+                // shear_stress[1] = shear_stress[1]  +mAppliedTangentTractions[fluid_site_index][1];
+                // shear_stress[2] = shear_stress[2]  + mAppliedTangentTractions[fluid_site_index][2];
+
+
             
                 counter+=1;
 			}
